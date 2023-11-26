4b8b1f7503443047613139421ccbd5f01b041399
Sebastian Quito <josue.quito@udla.edu.ec>
2023-11-26 18:05:35 -0500

    modified ListaProductos component to create and render a list of ProductoCards with the data obtained

--- components/home/listaProductos/ListaProductos.jsx
+++ components/home/listaProductos/ListaProductos.jsx
@@ -1,9 +1,19 @@
-import {Text} from "react-native";
+import ProductoCard from "../productoCard/ProductoCard";
+import {View} from "react-native";
 import styles from "./listaProductos.style";
+import {router} from "expo-router";
 
 const ListaProductos = ({data}) => {
     return (
-        <Text>Lista Productos</Text>
+        <View style={styles.cardsContainer}>
+            {data?.map((producto) => (
+                <ProductoCard
+                    key={producto.idProducto}
+                    producto={producto}
+                    handleNavigate={() => router.push(`/producto/${producto.idProducto}`)}
+                />
+            ))}
+        </View>
     )
 }
 
--- components/home/listaProductos/listaProductos.style.js
+++ components/home/listaProductos/listaProductos.style.js
@@ -1,7 +1,11 @@
 import {StyleSheet} from "react-native";
+import {SIZES} from "../../../constants";
 
 const styles = StyleSheet.create({
-
+    cardsContainer: {
+        marginTop: SIZES.medium,
+        gap: SIZES.small,
+    },
 });
 
 export default styles;
