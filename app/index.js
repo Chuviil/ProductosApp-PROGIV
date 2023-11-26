19f0edbefd2712ea380e9888aa3bcfb71338406d
Sebastian Quito <josue.quito@udla.edu.ec>
2023-11-26 16:51:24 -0500

    created basic structure for index page

--- app/index.js
+++ app/index.js
@@ -1,34 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
+import {SafeAreaView, View} from "react-native";
+import {Stack} from "expo-router";
+import {StatusBar} from "expo-status-bar";
+import ListaProductos from "../components/home/listaProductos/ListaProductos";
 
 export default function Page() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
-    </View>
-  );
+    return (
+        <SafeAreaView>
+            <Stack.Screen
+                options={{
+                    headerTitle: "Productos",
+                    headerTitleAlign: "center",
+                }}
+            />
+            <StatusBar style={"auto"}/>
+            <View>
+                <ListaProductos/>
+            </View>
+        </SafeAreaView>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
---
+++ components/home/listaProductos/ListaProductos.jsx
@@ -0,0 +1,10 @@
+import {Text} from "react-native";
+import styles from "./listaProductos.style";
+
+const ListaProductos = ({data}) => {
+    return (
+        <Text>Lista Productos</Text>
+    )
+}
+
+export default ListaProductos;
