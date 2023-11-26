d1676e3a4c5aecf173c95646fc160d140ce84977
Sebastian Quito <josue.quito@udla.edu.ec>
2023-11-26 18:25:00 -0500

    modified producto/[id] page to fetch data and use it on ProductoDisplay component

--- app/producto/[id].js
+++ app/producto/[id].js
@@ -1,10 +1,40 @@
-import {View, Text} from "react-native";
+import {ActivityIndicator, SafeAreaView, ScrollView, Text, View} from "react-native";
+import {Stack, useLocalSearchParams} from "expo-router";
+import {StatusBar} from "expo-status-bar";
+import {SIZES} from "../../constants";
+import useFetch from "../../hooks/useFetch";
+import ProductoDisplay from "../../components/producto/productoDisplay/ProductoDisplay";
 
 const Producto = () => {
+    const {id} = useLocalSearchParams();
+    const {data, loading, error} = useFetch(`Producto/${id}`);
+
     return (
-        <View style={styles.container}>
-            <Text>Producto</Text>
-        </View>
+        <SafeAreaView style={{flex: 1}}>
+            <Stack.Screen
+                options={{
+                    headerTitle: "Productos",
+                    headerTitleAlign: "center",
+                }}
+            />
+            <StatusBar style={"auto"}/>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View
+                    style={{
+                        flex: 1,
+                        padding: SIZES.medium,
+                    }}
+                >
+                    {loading ? (
+                        <ActivityIndicator size={"large"}/>
+                    ) : error ? (
+                        <Text>Error: {error.message}</Text>
+                    ) : (
+                        <ProductoDisplay producto={data} />
+                    )}
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 }
 
